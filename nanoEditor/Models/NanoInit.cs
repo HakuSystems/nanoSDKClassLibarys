152908c23049c5e5df3527f42c1bb657ff5ccb8c
HakuSystems <66133638+HakuSystems@users.noreply.github.com>
2023-03-30 22:24:31 +0200

    Added InitializeOnLoad Backup

---
+++ nanoEditor/Models/NanoInit.cs
@@ -0,0 +1,19 @@
+﻿using nanoEditor.Configs;
+using nanoEditor.Logger;
+using nanoEditor.UserInterfaces.Tools;
+using UnityEditor;
+using UnityEngine;
+
+namespace nanoEditor.Models;
+[InitializeOnLoad]
+public class NanoInit
+{
+    private static readonly ConfigManager _configManager = new();
+    public NanoInit()
+    {
+        NanoLog.Log("NANOSDK","nanoSDK Initialized");
+        if(_configManager.Config.BackupManager.AutoBackup)
+            BackupManager.CreateBackup(_configManager.Config.BackupManager.SaveAsUnitypackage,
+                _configManager.Config.BackupManager.DeleteOldBackups);
+    }
+}
